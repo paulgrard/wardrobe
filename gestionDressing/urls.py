7d707a498ea055db50349f6e3dd4db295cec19b5
paul <paulg@outlook.fr>
2015-12-23 14:08:42 +0100

    mise en place partie admin-correction urls de gestionDressing

---
+++ gestionDressing/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+from . import views
+
+urlpatterns = patterns('gestionDressing.views',
+    #url(r'^$', 'connexion', name='connexion'),
+    #url(r'^deconnexion$', 'deconnexion', name='deconnexion'),
+    #url(r'^bonjour$', 'dire_bonjour', name='bonjour'),
+)
+
