4b5889c8b942f9fcc0f15d69fb37318cc4a5c905
paul <paulg@outlook.fr>
2016-01-10 15:30:50 +0100

    Changement du contenu du JSON (message et success) dans dressingManage

--- dressingManage/views.py
+++ dressingManage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from dressingManage.forms import AddClotheForm, forms
 from django.contrib.auth.models import User
 from dressingManage.models import Clothe, Category, Color, Theme
@@ -16,73 +16,91 @@ def accueil(request):
 #vérifier si la couleur existe déja et les contraindre à 3 couleurs maxi
 #couleur joker
 def addClothe(request):
-    data = []
+    data = {}
+    success = False
     themes = []
     currentUser = request.user
-    if request.method == "POST":
-        form = AddClotheForm(request.POST, user=request.user)
-        if form.is_valid():
-            warmthC = form.cleaned_data["warmth"]
-            photoC = form.cleaned_data["photo"]
-            categoryC = Category.objects.get(name = form.cleaned_data["category"], area = form.cleaned_data["area"])
-            themesC = form.cleaned_data["themes"]
-            colorsC = form.cleaned_data["color"]
 
-            if currentUser.is_authenticated():
+    if currentUser.is_authenticated():
+        if request.method == "POST":
+            form = AddClotheForm(request.POST, user=request.user)
+            if form.is_valid():
+                warmthC = form.cleaned_data["warmth"]
+                photoC = form.cleaned_data["photo"]
+                categoryC = Category.objects.get(name = form.cleaned_data["category"], area = form.cleaned_data["area"])
+                themesC = form.cleaned_data["themes"]
+                colorsC = form.cleaned_data["color"]
+
                 newClothe = Clothe(warmth = warmthC, photo = photoC, state = 0, nbreUse = 0, category = categoryC, user = currentUser)
                 newClothe.save()
                 if themesC:
                     newClothe.themes.add(themesC)
-                
+                    
                 #for valColor in colorsC:
                 if colorsC:
                     col = Color(color = colorsC)#color = valColor
                     col.save()
                     newClothe.colors.add(col)
-                
+                    
                 '''yolo=newClothe.colors.all()
                 for x in yolo:
                     yolo2 = x.color
                 data = {'new_clothe':yolo2}
                 return HttpResponse(json.dumps(data), content_type='application/json')'''
-                
+                    
                 if newClothe:
-                    data = {'new_clothe':newClothe.photo}
+                    success = True
                 else:
-                    data = {'new_clothe':'Error during creation of clothe'}
+                    data['message'] = 'Error during creation of clothe'
+                        
+                
+
+            else: # si form non valide
+                data['message'] = 'Form not validated'
+                
+            
+            #return HttpResponse(json.dumps(data), content_type='application/json')
+
+        else: #si non post
+
+            ####################
+            if currentUser.is_authenticated():
+                form = AddClotheForm(user=request.user)
+                '''themesFromUser = Theme.objects.filter(userOwner = currentUser)
+                for theme in themesFromUser:
+                    themes.append(theme.name)
+                data = {'themes':themes}'''
             else:
-                data = {'new_clothe':'User is not authenticate'}
-
-            return HttpResponse(json.dumps(data), content_type='application/json')
+                return HttpResponseForbidden('User is not authenticated')
+            ####################
+            
+            data['message'] = 'Need a POST request'
+            data['success'] = success
+            
     else:
-        
-        if currentUser.is_authenticated():
-            form = AddClotheForm(user=request.user)
-            themesFromUser = Theme.objects.filter(userOwner = currentUser)
-            for theme in themesFromUser:
-                themes.append(theme.name)
-            data = {'themes':themes}
-        else:
-            data = {'new_clothe':'User is not authenticate'}
-        
+        return HttpResponseForbidden('User is not authenticated')
 
+    data['success'] = success
     #return render(request, 'dressingManage/addClothe.html', locals())
     return HttpResponse(json.dumps(data), content_type='application/json')
 
 
 def getAllClothes(request):
-    data = []
+    data = {}
+    success = False
     clothes = []
     photos = []
     currentUser = request.user
     if currentUser.is_authenticated():
         clothesFromUser = Clothe.objects.filter(user = currentUser)
         for clothe in clothesFromUser:
-            photos.append(clothe.photo)
-        data = {'clothes':photos}
+            photos.append(clothe.pk)
+        data['clothes'] = photos
+        success = True
     else:
-        data = {'clothe':'User is not authenticate'}
-        
+        return HttpResponseForbidden('User is not authenticated')
+
+    data['success'] = success
     return HttpResponse(json.dumps(data), content_type='application/json')
 
 #faire un form pour passer string
@@ -105,7 +123,8 @@ def createTheme(request):
     return HttpResponse(json.dumps(data), content_type='application/json')
 
 def getThemes(request):
-    data = []
+    data = {}
+    success = False
     themes = []
     currentUser = request.user
     
@@ -113,10 +132,12 @@ def getThemes(request):
         themesFromUser = Theme.objects.filter(userOwner = currentUser)
         for theme in themesFromUser:
             themes.append(theme.name)
-        data = {'themes':themes}
+        data['themes'] = themes
+        success = True
     else:
-        data = {'new_clothe':'User is not authenticate'}
+        return HttpResponseForbidden('User is not authenticated')
         
-
+    data['success'] = success
+    
     #return render(request, 'dressingManage/addClothe.html', locals())
     return HttpResponse(json.dumps(data), content_type='application/json')
